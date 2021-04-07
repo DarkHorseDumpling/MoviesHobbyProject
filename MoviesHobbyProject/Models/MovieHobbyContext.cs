cbdd8db5ab5280d77ec4b5113538c649de5043aa
Jasrah85 <Alainna.nichols85@gmail.com>
2021-04-06 22:48:25 -0400

    Untested Migration file

---
+++ MoviesHobbyProject/Models/MovieHobbyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesHobbyProject.Models
+{
+    public class MovieHobbyContext : DbContext
+    {
+        public MovieHobbyContext(DbContextOptions<MovieHobbyContext> options) : base(options)
+        { }
+
+    }
+}
