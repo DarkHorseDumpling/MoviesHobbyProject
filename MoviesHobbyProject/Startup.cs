470d5183d951e1c16c5fcfdf89a6ff8d0e13c18a
Jasrah85 <Alainna.nichols85@gmail.com>
2021-04-16 18:50:24 -0400

    Added Movie input form and details page for db stored movies.

--- MoviesHobbyProject/Startup.cs
+++ MoviesHobbyProject/Startup.cs
@@ -55,7 +55,7 @@ namespace MoviesHobbyProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
             });
         }
     }
